7c97adedecf5c9d69355ec4ad59e0c3b8ddc6912
Stephen Catsamas <seca8193@gmail.com>
2019-05-02 09:40:35 +1000

    Switched power pins of ec from analogue to ground

--- Dandenong_SIM2_Dev/Dandenong_SIM2_Dev.ino
+++ Dandenong_SIM2_Dev/Dandenong_SIM2_Dev.ino
@@ -6,25 +6,21 @@
 
  *** series memory usage issue which may lead to faulty readings and SD card issue
  ******************************************************************************/
+//setup quick scan and record function from website...so it reads a file and then enters a quick scan and upload sequence until that file is changed again.
 
 #include <Wire.h>
 #include <SparkFun_MS5803_I2C.h>
 #include <SoftwareSerial.h>
-#include <BatLevel.h>
 #include <JeeLib.h> // Low power functions library
 ISR(WDT_vect) { Sleepy::watchdogEvent(); } // Setup the watchdog
 #include <avr/wdt.h>
 
 SoftwareSerial Sim900(2,3);
 
-//Declaring variables to store results
-float EC, EC_Var, lastEC, Pressure, lastPressure, EC_sum, Pressure_sum, batLev;
-float ECDiff, TempDiff, PressureDiff, FasterA;
-short Counter, DailyCounter;
-float Temp, lastTemp, Temp_sum, fasterA, ForceLog;
+float temp, EC, Depth, TempVar, ECVar, DepthVar, lastECwritten, lastTempwritten, lastDepthwritten, ECDiff, TempDiff, DepthDiff, FasterA, ECThresholdForForcedLoggingAtMinInterval;
+short counter, DailyCounter;
 int a, ScanInterval, MinLogInterval, MaxLogInterval, Writer;
 String AAA, BBB;
-String SiteID = "developement";
 unsigned long previous;
 uint8_t x, answer;
 char response[100];
@@ -36,126 +32,115 @@ char response[100];
 //  ADDRESS_LOW  = 0x77
 MS5803 depth_sensor(ADDRESS_HIGH);
 
-//create battery object:
-BatLevel batlv;
-
-//Sensor pints
-int ECInput = A1;
-int ECPower = 7;
-
 void setup() {
-  // put your setup code here, to run once:
-  ScanInterval = 10; //set the approximate seconds between each scan
+  ScanInterval = 10; //seconds
   MinLogInterval = 60; //seconds
   MaxLogInterval = 3600; //seconds
-
-  //this is for autologging function at SCAN interval! Just place a 100ohm 
-  //shunt from 5v to A2 and it should enter into this logging
-  pinMode(A2, INPUT); 
-
+  ECThresholdForForcedLoggingAtMinInterval = 0.8;
+  pinMode(A2,INPUT); //this is for autologging function at SCAN interval! Just place a 100ohm shunt from 5v to A5 and it should enter into this logging
+  
   pinMode(2,INPUT);
   pinMode(3,OUTPUT);
   digitalWrite(2,LOW);
   digitalWrite(3,LOW);
 
-  //EC sensor
-  pinMode(ECInput, INPUT);
-  pinMode(ECPower, OUTPUT);
-  digitalWrite(ECPower,LOW);
+  //Probe stuff
+  pinMode(A0,INPUT); //EC PIN is A0
+  pinMode(A3,OUTPUT);//EC Power is A3
+  pinMode(A1,OUTPUT);//EC Ground is A1
+  digitalWrite(A1,LOW);//EC Ground is set to low 
 
-  //Serial communication
   Serial.begin(19200);
   Sim900.begin(19200);
   depth_sensor.begin();
-
-
-  Temp_sum = 0.0;
-  EC_sum = 0.0;
-  Pressure_sum = 0.0;
-
+  
+  temp = 0.0;
+  EC = 0.0;
+  Depth = 0.0;
+  
   //go and get temperature and EC a couple of times to initiate things!
-  GetSensorData();
+  GetTempANDEC();
   delay(1000);
-  GetSensorData();
+  GetTempANDEC();
 
   //set placeholdernumbers
-  //ForceLog = 1.01 * Pressure;
-  lastEC = EC;
-  lastTemp = Temp;
-  lastPressure = Pressure;
-  Temp_sum = Temp;
-  EC_sum = EC;
-  Pressure_sum = Pressure;
+  lastECwritten = ECVar;
+  lastTempwritten = TempVar;
+  lastDepthwritten = DepthVar;
+  EC = ECVar;
+  temp = TempVar;
+  Depth = DepthVar;
 
   //set Ec and temp
   SendToWeb222(1);
 
-  Temp_sum = 0.0;
-  EC_sum = 0.0;
-  Pressure_sum = 0.0;
+  EC = 0.0;
+  temp = 0.0;
+  Depth = 0.0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   //turn off the TX and RX pins so the RX and TX pins of the SIM go low - saves power!
   digitalWrite(2,LOW);
   digitalWrite(3,LOW);
+
   //get sensor of A5 to see if we need to do quick things
   FasterA = analogRead(A2)*1.0;
 
-  //Get sensor data
-  GetSensorData();
+  //go and get temperature and EC
+  GetTempANDEC();
+
   //add these values to the averagers
-  EC_sum = EC_sum + EC;
-  Temp_sum = Temp_sum + Temp;
-  Pressure_sum = Pressure_sum + Pressure;
+  EC = EC + ECVar;
+  temp = temp + TempVar;
+  Depth = Depth + DepthVar;
 
   //increment counters
-  Counter = Counter + ScanInterval;
+  counter = counter + ScanInterval;
   DailyCounter = DailyCounter + ScanInterval;
 
   //if we have gone for 60 seconds, record the data.
-  if(Counter >= MinLogInterval || FasterA>900) {
-    EC_sum = EC_sum / (Counter/ScanInterval);
-    Temp_sum = Temp_sum / (Counter/ScanInterval);
-    Pressure_sum = Pressure_sum / (Counter/ScanInterval);
+  if(counter >= MinLogInterval || FasterA>900) {
+    EC = EC / (counter/ScanInterval);
+    temp = temp / (counter/ScanInterval);
+    Depth = Depth / (counter/ScanInterval);
 
     Serial.print(F(". It has been more than Min Log Interval. Previous average EC: "));
-    Serial.print(lastEC);
+    Serial.print(lastECwritten);
     Serial.print(F(", Previous average Temp: "));
-    Serial.print(lastTemp);
-    Serial.print(F(", Previous average Pressure: "));
-    Serial.print(lastPressure);
+    Serial.print(lastTempwritten);
     
     Serial.print(F("Average EC: "));
-    Serial.print(EC_sum);
+    Serial.print(EC);
     Serial.print(F(", Average Temp: "));
-    Serial.print(Temp_sum);
-    Serial.print(F(", Average Pressure: "));
-    Serial.print(Pressure_sum);
+    Serial.print(temp);
+    Serial.print(F(", Average Depth: "));
+    Serial.print(Depth);
 
     ECDiff = 0.00001;
-    ECDiff = ((EC_sum*1.0-lastEC*1.0)/(lastEC*1.0+0.00001));
+    ECDiff = ((EC*1.0-lastECwritten*1.0)/(lastECwritten*1.0+0.00001));
     ECDiff= (ECDiff*ECDiff);
     ECDiff = sqrt(ECDiff*1.0);
     TempDiff = 0.00001;
-    TempDiff = ((Temp_sum*1.0-lastTemp*1.0)/(lastTemp*1.0+0.00001));
+    TempDiff = ((temp*1.0-lastTempwritten*1.0)/(lastTempwritten*1.0+0.00001));
     TempDiff = (TempDiff*TempDiff);
     TempDiff = sqrt(TempDiff*1.0);
-    PressureDiff = 0.00001;
-    //PressureDiff = ((Pressure_sum*1.0-lastPressure*1.0)/(lastPressure*1.0+0.00001));
-    PressureDiff = Pressure_sum*1.0-lastPressure*1.0;
-    PressureDiff = (PressureDiff*PressureDiff);
-    PressureDiff = sqrt(PressureDiff*1.0);
-
+    DepthDiff = 0.00001;
+    //DepthDiff = ((Depth*1.0-lastDepthwritten*1.0)/(lastDepthwritten*1.0+0.00001));
+    DepthDiff = Depth*1.0 - lastDepthwritten*1.0;
+    DepthDiff = (DepthDiff*DepthDiff);
+    DepthDiff = sqrt(DepthDiff*1.0);
+    
     Serial.print(F(", EC Diff: "));
     Serial.print(ECDiff);
     Serial.print(F(", Temp Diff: "));
     Serial.print(TempDiff);
-    Serial.print(F(", Pressure Diff: "));
-    Serial.print(PressureDiff);
+    Serial.print(F(", Depth Diff: "));
+    Serial.print(DepthDiff);
     Serial.print(F(", DailyCounter: "));
     Serial.print(DailyCounter);
+    Serial.print(F(", EC: "));
+    Serial.print(EC);
     Serial.print(F(", FasterA: "));
     Serial.println(FasterA);
 
@@ -169,30 +154,26 @@ void loop() {
       Writer = 1;
       Serial.println(F("We will write because TempDIFF was greater than 10%"));
     }
-    if(PressureDiff>=1.0){
+    if(DepthDiff>=1.0){
       Writer = 1;
-      Serial.println(F("We will write because PressureDiff was greater than 1mbar"));
+      Serial.println(F("We will write because DepthDIFF was greater than 1 mbar"));
     }
     if(DailyCounter>=MaxLogInterval){
       Writer = 1;
       Serial.println(F("We will write because the time elapsed was greater than the max logging interval"));
     }
-    //if(Pressure_sum>=ForceLog){
-    //  Writer = 1;
-    //  Serial.println(F("We will log because the current EC value was greater than the threshold set for minimum logging intervals"));
-    //}    
     if(FasterA>=900.0){
       Writer = 1;
-      Serial.println(F("We will log because there is a jumper between A2 and 5v"));
-    }
-      
+      Serial.println(F("We will log because there is a jumper between A5 and 5v"));
+    }  
+    
     if(Writer==1){
       //ok we should send to the web because we have some changing datasets.
       //turn on the TX pin so the RX of the SIM goes high
       digitalWrite(3,HIGH);
-      lastEC = EC_sum;
-      lastTemp = Temp_sum;
-      lastPressure = Pressure_sum;
+      lastECwritten = EC;
+      lastTempwritten = temp;
+      lastDepthwritten = Depth;
       SendToWeb222(0);
       Serial.print(F("Sent to web. "));
       //turn off the TX and RX pins so the RX and TX pins of the SIM go low - saves power!
@@ -203,14 +184,15 @@ void loop() {
       //DailyCounter = 0;
     }
     //resent counters
-    Counter = 0;
-    EC_sum = 0;
-    Temp_sum = 0;
-    Pressure_sum = 0;
-  }
+    counter = 0;
+    EC = 0;
+    temp = 0;
+    Depth = 0;
+  } 
   Serial.println(", delaying.");
   Sleepy::loseSomeTime(1000 * ScanInterval);
-  //delay(1000 * ScanInterval);  
+  //delay(1000 * ScanInterval);
+
 }
 
 void SendToWeb222(int abc) {
@@ -254,14 +236,12 @@ void SendToWeb222(int abc) {
   do{a++;}while (sendATcommand(F("AT+CHTTPSOPSE=\"www.cartridgerefills.com.au\",80,1"), "OK",10000) == 0 && a<5);
 //you may need to move the /EoDC/ to the below line bewfore the "WriteMe.php... so it looks like "EoDC/WriteMe.php.... but try the way i have it first
   AAA = "GET /";
-  AAA += "EoDC/databases/WriteMe.php?SiteName="+SiteID+".csv&T=";
-  AAA += Temp_sum;
+  AAA += "EoDC/databases/WriteMe.php?SiteName=ECTEST.csv&T=";
+  AAA += temp;
   AAA += "&EC=";
-  AAA += EC_sum;
+  AAA += EC;
   AAA += "&D=";
-  AAA += Pressure_sum;
-  AAA += "&B=";
-  AAA += batLev;
+  AAA += Depth;
   AAA += " HTTP/1.1\r\nHost: www.cartridgerefills.com.au:80\r\n\r\n";
   BBB = "AT+CHTTPSSEND=";
   BBB +=AAA.length();
@@ -289,6 +269,76 @@ void SendToWeb222(int abc) {
    a++;
    delay(2000);
   } 
+   
+}
+
+void SendToWeb(int abc) {
+  a = 0;
+  if(abc==1){
+        //first check to see if the modem is still on - if it is, turn it off
+
+      while (sendATcommand(F("AT"),"OK",2000) == 1 && a < 5) {
+         TurnOnOffSim();
+         a++;
+         delay(2000);
+      } 
+       
+      //OK, it should now be off, now to turn it on
+      a=0;
+      while (sendATcommand(F("AT"),"OK",2000) == 0 && a<5) {
+         TurnOnOffSim();
+         a++;
+         delay(2000);
+      }
+     
+      //OK, the system should now be on. 
+      a=0;
+      do{a++;}while (sendATcommand(F("AT+IPR=19200"),"OK",10000) == 0 && a<5);
+      a=0;
+      do{a++;}while (sendATcommand(F("ATE0"),"OK",10000) == 0 && a<5);
+      a=0;
+      do{a++;}while (sendATcommand(F("AT&W0"),"OK",10000) == 0 && a<5);
+      a=0;
+      do{a++;}while (sendATcommand(F("AT+CGDCONT=1,\"IP\",\"mdata.net.au\",\"0.0.0.0\""), "OK",10000) == 0 && a<5);
+      a=0;
+      do{a++;}while (sendATcommand(F("AT+CGSOCKCONT=1,\"IP\",\"mdata.net.au\""), "OK",10000) == 0 && a<5);
+      a=0;
+      do{a++;}while (sendATcommand(F("AT+CSOCKSETPN=1"), "OK",10000) == 0 && a<10);
+      a=0;
+      do{a++;}while (sendATcommand(F("AT+CHTTPSSTART"), "OK",10000) == 0 && a<2);
+      a=0;
+  }
+  
+  do{a++;}while (sendATcommand(F("AT+CHTTPSOPSE=\"www.cartridgerefills.com.au\",80,1"), "OK",10000) == 0 && a<5);
+//you may need to move the /EoDC/ to the below line bewfore the "WriteMe.php... so it looks like "EoDC/WriteMe.php.... but try the way i have it first
+  AAA = "GET /";
+  AAA += "EoDC/databases/WriteMe.php?SiteName=ECTEST.csv&T=";
+  AAA += temp;
+  AAA += "&EC=";
+  AAA += EC;
+  AAA += "&D=";
+  AAA += Depth;
+  AAA += " HTTP/1.1\r\nHost: www.cartridgerefills.com.au:80\r\n\r\n";
+  BBB = "AT+CHTTPSSEND=";
+  BBB +=AAA.length();
+
+  a=0;
+  do{a++;}while (sendATcommand(BBB, ">",10000) == 0 && a<5);
+
+  a=0;
+  do{a++;}while (sendATcommand(AAA, "OK",10000) == 0 && a<5);
+
+  delay(1000);
+
+  a=0;
+  do{a++;}while (sendATcommand(F("AT+CHTTPSCLSE"), "OK",5000) == 0 && a<3);
+
+  //a=0;
+  //do{a++;}while (sendATcommand(F("AT+CHTTPSSTOP"), "OK",10000) == 0 && a<5);
+
+  //a=0;
+  //do{a++;}while (sendATcommand(F("AT+CPOF"), "OK",10000) == 0 && a<5);
+   
 }
 
 int8_t sendATcommand(String ATcommand, char* expected_answer1, unsigned int timeout){
@@ -329,7 +379,6 @@ int8_t sendATcommand(String ATcommand, char* expected_answer1, unsigned int time
 }
 
 
-
 void TurnOnOffSim() {
    digitalWrite(8,LOW);
    delay(1000);
@@ -339,35 +388,24 @@ void TurnOnOffSim() {
    delay(5000); 
 }
 
-void GetSensorData() {
-  //EC, Temp & Pressure
-  //EC_Var = 0.0;
-  
-  //digitalWrite(ECPower, HIGH);
-  delay(100);
-  EC_Var = analogRead(ECInput);
-   
-  batLev = batlv.readLev();
-  
-  Temp = depth_sensor.getTemperature(CELSIUS, ADC_512);
-  Pressure = depth_sensor.getPressure(ADC_4096)/10;
-  //Depth = (100*(Pressure - Pressure_base))/(997*9.8)*1000;
-  digitalWrite(ECPower, LOW);
-
-  //EC Transformation
-  //EC = EC_Var;
-  EC = 0;
-  //EC = map(EC_Var, 0, 1023, 0, 5000);
-  //EC = (EC_Var*1025)/(5-EC_Var); //1K resistor
-  //EC = EC_Var-25;
-  //EC = (1000/(EC_Var*2.88));
-  //EC = EC_Var / (1+ 0.019 *(Temp-25.0));
-
-  //Print
+void GetTempANDEC(){
+  //Temperature and Depth
+  TempVar = depth_sensor.getTemperature(CELSIUS, ADC_512);
+  DepthVar = depth_sensor.getPressure(ADC_4096)/10;
   Serial.print(F("Recorded Temperature = "));
-  Serial.print(Temp);
+  Serial.print(TempVar);
+  Serial.print(F("Recorded Depth = "));
+  Serial.print(DepthVar);
+  
+  //EC
+  ECVar = 0.0;
+  digitalWrite(A3,HIGH);
+  ECVar= analogRead(A0);
+  ECVar= analogRead(A0);// This is not a mistake, First reading will be low beause if charged a capacitor
+  digitalWrite(A3,LOW);
   Serial.print(F(", Recorded EC = "));
-  Serial.print(EC);
-  Serial.print(F(", Recorded Pressure = "));
-  Serial.print(Pressure);
+  Serial.print(ECVar);
+  Serial.print(F(", A5 reading = "));
+  Serial.print(FasterA);
+
 }
