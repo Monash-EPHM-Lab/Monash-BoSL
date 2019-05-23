2c1b4fa356d249002b6eb2a9aaaca5d3833f3346
Boris Deletic <borisdeletic0@gmail.com>
2019-05-23 21:14:29 +1000

    made some improvements and additions. Need to lab test EC sensor

--- EC.cpp
+++ EC.cpp
@@ -1,6 +1,6 @@
 #include <EC.h>
 
-EC::EC(uint8_t pinR, uint8_t pinA, uint8_t pinB, bool doSwitching) : 
+EC::EC(uint8_t pinR, uint8_t pinA, uint8_t pinB, bool doSwitching = false) : 
     doSwitch(doSwitching),
     pinRead(pinR),
     pinVA(pinA),
@@ -11,45 +11,58 @@ EC::EC(uint8_t pinR, uint8_t pinA, uint8_t pinB, bool doSwitching) :
     pinMode(pinVB,OUTPUT);//EC Ground is pinVB
     digitalWrite(pinVB,LOW);//EC Ground is set to low 
     digitalWrite(pinVA,LOW);
-
-    pinVAswitch = pinVA;
-    pinVBswitch = pinVB;
-    
+    storeLen = 0;
 }
 
  
-uint16_t EC::measure(){ // make sure shift works properly
+void EC::measure(){ 
   uint16_t ECVar = 0;
   
   for (uint8_t i = 0; i<2; i++){
-      digitalWrite(pinVBswitch,LOW);
-      digitalWrite(pinVAswitch,HIGH);
+      digitalWrite(pinVB,LOW);
+      digitalWrite(pinVA,HIGH);
       delay(100);//this value may need to be adjusted
       ECVar += ECread();
-      digitalWrite(pinVBswitch,LOW);
-      digitalWrite(pinVAswitch,LOW);
+      digitalWrite(pinVB,LOW);
+      digitalWrite(pinVA,LOW);
       pinSwitch();
   }
   ECVar = ECVar/2;
-  return(ECVar);
+
+  ECVals[storeLen] = reading;
+  storeLen++;
+}
+
+float getEC(clearVals = true) {
+  uint16_t ECSum = 0;
+  for (int i = 0; i<storeLen; i++) {
+    ECSum += ECVals[i];
+  }
+
+  float ECAverage = (float)ECSum / (float)storeLen;
+
+  return ECAverage
 }
 
 void EC::pinSwitch(){
-   if (doSwitch == true){
-   pinSwitcher = pinVAswitch;
-   pinVAswitch = pinVBswitch;
-   pinVBswitch = pinSwitcher;
-   polarity = !polarity;
-   }
+  if (doSwitch) {
+    uint8_t temp = pinVA;
+    pinVA = pinVB;
+    pinVB = temp;
+    polarity = !polarity;
+  }
 }
 
 uint16_t EC::ECread(){
    uint16_t readMax = 1023;
    uint16_t ECVal = analogRead(pinRead);
-   if (polarity == true){
+   if (polarity) {
        ECVal = readMax - ECVal;
    }
    Serial.print("ECEC ");
    Serial.println(ECVal);
    return ECVal;
 }
+
+
+
--- EC.h
+++ EC.h
@@ -6,23 +6,24 @@
  class EC
  {
      public:
-        EC(uint8_t pinR, uint8_t pinA, uint8_t pinB, bool doSwitching = true);
+        EC(uint8_t pinR, uint8_t pinA, uint8_t pinB, bool doSwitching = false);
         //arguments: read pin, voltage pin A, voltage pin B, do polarity switching.
         uint16_t measure(void);
-        void pinSwitch(void);
-        
-        
+          
      private:
-        bool doSwitch;
-        const uint8_t pinVA; 
-        const uint8_t pinVB;
-        uint8_t pinVAswitch; 
-        uint8_t pinVBswitch;
-        uint8_t pinSwitcher;
-        const uint8_t pinRead;
-        bool polarity = false;
+        void pinSwitch(void);
         uint16_t ECread(void);
+        float getEC(clearVals = true;);
 
+        uint16_t ECVals[10];  // array to store multiple EC measuremts, averaged on getValue call (each minute)
+        uint8_t storeLen;
+
+        uint8_t pinVA; 
+        uint8_t pinVB;
+        const uint8_t pinRead;
+
+        bool doSwitch;
+        bool polarity = false;
 };
  
  #endif
