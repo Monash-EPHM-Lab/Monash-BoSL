fc9b4448215c8be29f95cf9dd0cef46a57608d1e
StephenCatsamas <seca8193@gmail.com>
2020-03-09 11:14:38 +1100

    added test for fix fft library

--- Velocity/Heterodyne/FFTLib/FFTLib.ino
+++ Velocity/Heterodyne/FFTLib/FFTLib.ino
@@ -1,79 +1,53 @@
 
-//FHT library
-#include <fix_fft.h>
-#include "src\I2C.h"
+//FFT library
+//#include "src\arduinoFFTfix.h"
+#include <arduinoFFT.h>
 
 
-int16_t f[128];
+double source[128] = {0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1};
+
+//FFT sample buffers
+double read[128];
+double imag[128];
+
+ 
+//Define FFT object
+arduinoFFT FFT = arduinoFFT(); 
 
 void setup(){
-   I2c.begin();
-   Serial.begin(230400);
+
+   Serial.begin(115200);
 }
 
 void loop(){
-	
-	sampleFast();
 
-	   
-	   fix_fftr(f, 128, 0);
-	   
-	   for(int i=4; i<(128/2); i++)
-    {
-        Serial.println(f[i]);   
-    }
+		for(int i = 0; i < 128; i++){
+			imag[i] = 0;
+			read[i] = source[i];
+		}
+		
+		//Compute FFT
+		FFT.Windowing(read, 128, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+		FFT.Compute(read, imag, 128, FFT_FORWARD);
+		FFT.ComplexToMagnitude(read, imag, 128);
 
-		for(int i=0; i<(65); i++)
-	   {
-			Serial.println(0);   
-	   }
-//	plotFFT();
-
-	clearPlot();
-    
+		plotFFT();
+		clearPlot();
+		
 }
-// Plot FFT
-
-void getVel(int velMulti, int averages){
-
-	   
-}
-
-
-void sampleFast(){
-	uint8_t adcData[256];
-	
-	I2c.write(54, 0b11011100);
-	I2c.write(54, 0b00000010);
-	
-	I2c.read(54,255,adcData);
-	
-	for(int i = 0; i <254; i += 2){
-	int rsult = (adcData[i]-240)*256 + adcData[i+1];
-	if (rsult > 2048){
-		rsult = rsult - 4096;
-	}
-	f[i/2] = 8*rsult;
-	}
-	
-	int rsult = (adcData[254]-240)*256 + adcData[253];
-	if (rsult > 2048){
-		rsult = rsult - 4096;
-	}
-	f[127] = 8*rsult;
-	
-}
-
 
 
 void plotFFT(){
-	
+	for(int i=2; i<(128/2); i++)
+    {
+        Serial.println(read[i], 1);   
+    }
+  
 }
-//TODO ADD GOOD AVERAGEING/DATA STUFF
 
 
 void clearPlot(){
-	delay(1500);
+	delay(3000);
 	for(int i=0; i<500; i++)
    {
         Serial.println(0);   
